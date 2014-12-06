803dd7a4d73806b3c0a83fdf9c4ab589bdc7dc38
absimas <ab.simas@gmail.com>
2014-12-06 14:25:40 +0200

    User class added

---
+++ Models/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  ApplePoker
+//
+//  Created by Simas Abramovas on 12/6/14.
+//  Copyright (c) 2014 Simas Abramovas. All rights reserved.
+//
+
+import Foundation
+
+class User {
+
+    class func createUser(username: String, callback: (() -> (error: String, user: User?)) -> Void) {
+        NSUrl = "http://applegame.herokuapp.com" 
+    }
+    
+}
