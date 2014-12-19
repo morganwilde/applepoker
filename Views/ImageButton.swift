bd0c8e9361e9fadc8b0c4b4f21e73e7d3d3bf0f1
AroSaneD <mail.junk.3135@gmail.com>
2014-12-19 12:51:35 +0200

    Fixed the damn cell image view

---
+++ Views/ImageButton.swift
@@ -0,0 +1,11 @@
+//
+//  ImageButton.swift
+//  ApplePoker
+//
+//  Created by Arūnas Seniucas on 12/19/14.
+//  Copyright (c) 2014 Arūnas Seniucas. All rights reserved.
+//
+
+import UIKit
+
+class ImageButton:
