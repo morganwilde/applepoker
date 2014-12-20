9538ab243c7004e0f145069a820a8878e4130be6
Morgan Wilde <wilde.morgan@gmail.com>
2014-12-20 14:20:58 +0200

    Added Regex into helpers

---
+++ Helpers.swift
@@ -0,0 +1,25 @@
+//
+//  Helpers.swift
+//  ApplePoker
+//
+//  Created by Morgan Wilde on 20/12/2014.
+//  Copyright (c) 2014 Morgan Wilde. All rights reserved.
+//
+
+import Foundation
+
+class Regex {
+    let internalExpression: NSRegularExpression
+    let pattern: String
+    
+    init(_ pattern: String) {
+        self.pattern = pattern
+        var error: NSError?
+        self.internalExpression = NSRegularExpression(pattern: pattern, options: .CaseInsensitive, error: &error)!
+    }
+    
+    func test(input: String) -> Bool {
+        let matches = self.internalExpression.matchesInString(input, options: nil, range:NSMakeRange(0, countElements(input)))
+        return matches.count > 0
+    }
+}
