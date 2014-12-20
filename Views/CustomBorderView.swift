c0c9808a8b6c02e94621b533535c0465b48ebe9f
AroSaneD <mail.junk.3135@gmail.com>
2014-12-20 13:13:19 +0200

    recent changes

--- Views/CustomBorderView.swift
+++ Views/CustomBorderView.swift
@@ -1,21 +1,18 @@
 //
-//  Card.swift
+//  CustomBorderView.swift
 //  ApplePoker
 //
-//  Created by Arūnas Seniucas on 12/6/14.
+//  Created by Arūnas Seniucas on 12/20/14.
 //  Copyright (c) 2014 Arūnas Seniucas. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
-class cards: UIView{
+class CustomBorderView:UIView {
     var background = UIBezierPath()
     
     override func drawRect(rect: CGRect) {
-        var background = UIBezierPath()
-        
-        
-    }
     
+    }
+
 }
