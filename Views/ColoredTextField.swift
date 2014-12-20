83698a1df46b9b72991f951f7e1883ee497b29d4
AroSaneD <mail.junk.3135@gmail.com>
2014-12-20 11:47:13 +0200

    ddd

--- Views/ColoredTextField.swift
+++ Views/ColoredTextField.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
-class BorderedTextField: UITextField {
+class ColoredTextField: UITextField {
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
+        var borderColor = UIColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0)
+        
+        self.layer.cornerRadius = 10
+        self.layer.borderWidth = 2
+        self.layer.borderColor = borderColor.CGColor
         
     }
 }
